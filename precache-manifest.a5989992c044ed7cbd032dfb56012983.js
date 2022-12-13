4c3c9ff4bf69b324ca6a055fe8fe6730cb306c2c
AlouiBassem <BASSEM0ALOUI@GMAIL.COM>
2022-12-13 16:32:26 +0100

    Updates

---
+++ precache-manifest.a5989992c044ed7cbd032dfb56012983.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c345515c570369c8b49b",
+    "url": "/react/static/css/main.56132ba7.chunk.css"
+  },
+  {
+    "revision": "c345515c570369c8b49b",
+    "url": "/react/static/js/main.c345515c.chunk.js"
+  },
+  {
+    "revision": "2c77b0a4cfebaa993647",
+    "url": "/react/static/js/1.2c77b0a4.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "a55a3219238e3c7c3f7dfc823ad8f3fa",
+    "url": "/react/static/media/Bassem ALOUI - DEV.a55a3219.pdf"
+  },
+  {
+    "revision": "bbd33a6e28c65a5c3210458181524af0",
+    "url": "/react/static/media/me1.bbd33a6e.jpg"
+  },
+  {
+    "revision": "c5ff27578503c6f5aa91cd105671949b",
+    "url": "/react/static/media/react2.c5ff2757.jpg"
+  },
+  {
+    "revision": "b8a3d3d299736576085270e2eaa00ea1",
+    "url": "/react/static/media/portcap.b8a3d3d2.png"
+  },
+  {
+    "revision": "912537743ff2fa394115f0e6204e2206",
+    "url": "/react/static/media/Secondimg.91253774.jpg"
+  },
+  {
+    "revision": "e92ac0a28f0482876733edb7fce3e807",
+    "url": "/react/index.html"
+  }
+];
